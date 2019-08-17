d0f32b4c3baab61a363d54c3a6d89e116d6af72f
Anderson Ismael <23392311+aicouto@users.noreply.github.com>
2019-08-17 15:49:22 -0300

    barra adicionada

--- controller.php
+++ controller.php
@@ -3,7 +3,7 @@ function controller($nomeStr,$data=null){
   $arr=explode('/',$nomeStr);
   $nomeDoRepositorioStr=@$arr[0];
   $nomeDoPacoteStr=@$arr[1];
-  $str=ROOT.'call/callgnomo'.$nomeDoRepositorioStr;
+  $str=ROOT.'call/callgnomo/'.$nomeDoRepositorioStr;
   $str.='/controller/'.$nomeDoPacoteStr.'.php';
   if(file_exists($str)){
     if(is_array($data)){
