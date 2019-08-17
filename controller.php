d8fedbfc7847b0fd87b2f0907578c01a7e272342
Anderson Ismael <23392311+aicouto@users.noreply.github.com>
2019-08-17 09:27:14 -0300

    pronto para ser testado

--- controller.php
+++ controller.php
@@ -1,1 +1,16 @@
-
+<?php
+function controller($nomeStr,$data=null){
+  $arr=explode('/',$nomeStr);
+  $nomeDoRepositorioStr=@$arr[0];
+  $nameDoPacoteStr=@$arr[1];
+  $str=ROOT.'call/callgnomo'.$nomeDoRepositorioStr;
+  $str.='/controller/'.$nomeDoPacoteStr.'.php';
+  if(file_exists($str)){
+    if(is_array($data)){
+      extract($data);
+    }
+    return require_once($str);
+  }else{
+    die('o controller '.$str.' não existe');
+  }
+}
