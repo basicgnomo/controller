1f8b0f32bd24ae441cb101702a2f4f2eb0f81f97
Anderson Ismael <23392311+aicouto@users.noreply.github.com>
2019-08-30 16:03:53 -0300

    removi as referências ao call

--- controller.php
+++ controller.php
@@ -3,7 +3,8 @@ function controller($nomeStr,$data=null){
   $arr=explode('/',$nomeStr);
   $nomeDoRepositorioStr=@$arr[0];
   $nomeDoPacoteStr=@$arr[1];
-  $str=ROOT.'call/callgnomo/'.$nomeDoRepositorioStr;
+  //$str=ROOT.'call/callgnomo/'.$nomeDoRepositorioStr;
+  $str=ROOT.$nomeDoRepositorioStr;
   $str.='/controller/'.$nomeDoPacoteStr.'.php';
   if(file_exists($str)){
     if(is_array($data)){
